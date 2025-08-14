cce24a49c6cf631ee6879b1f8dd0a549102ef71e
bitecare <130420337+bitecare@users.noreply.github.com>
2025-08-14 10:28:58 +0900

    [Silver I] Title: IOIOI, Time: 308 ms, Memory: 33388 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,16 +1,23 @@
 import sys
 input = sys.stdin.readline
 
-num = int(input())
-lengths = int(input())
-
-words = input()
-pattern = "IOI" + "OI" * (num-1)
+n = int(input())
+m = int(input())
+s = input()
 
 cnt = 0
+i = 0
+repeat = 0
 
-for i in range(lengths - len(pattern) + 1):
-    if words[i] == "I":
-        if words[i : i + len(pattern)] == pattern :
+while i < m - 2:
+    if s[i : i + 3] == "IOI":
+        repeat +=1
+        if repeat >= n:
             cnt +=1
+        i+=2
+        
+    else:
+        repeat = 0
+        i+=1
+        
 print(cnt)
